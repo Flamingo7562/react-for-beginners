be8ce05e5609cd4fa169d0850afb4572b549b9bd
Flamingo <Flamingo7562@gmail.com>
2025-01-20 18:49:56 +0900

    Detail Components

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,44 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 function Detail() {
-  return <h1>Detail</h1>;
+  const { id } = useParams();
+  const [myjson, setMyjson] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    console.log(json.data.movie);
+    setMyjson(json.data.movie);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
+
+  return (
+    <div>
+      <h1>Detail</h1>
+      <hr></hr>
+      {loading ? (
+        <div>Hello! Now Loading...</div>
+      ) : (
+        <div>
+          <img src={myjson.medium_cover_image} />
+          <h2>{myjson.title}</h2>
+          <h3>Genres</h3>
+          <ul>
+            {myjson.genres.map((g) => (
+              <li key={g}>{g}</li>
+            ))}
+          </ul>
+          <p>{myjson.date_uploaded}</p>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Detail;
