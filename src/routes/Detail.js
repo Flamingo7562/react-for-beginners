82e89a8fb4f8f633ee849a32d2540763c7b3bfcf
Flamingo <Flamingo7562@gmail.com>
2025-01-23 22:50:06 +0900

    Why mySummary don't work?

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import style from "../styles/Detail.module.css";
 
 function Detail() {
   const { id } = useParams();
   const [myjson, setMyjson] = useState({});
   const [loading, setLoading] = useState(true);
+  //const [mySummary, setMySummary] = useState(null);
 
   const getMovie = async () => {
     const json = await (
@@ -12,29 +14,37 @@ function Detail() {
     ).json();
     console.log(json.data.movie);
     setMyjson(json.data.movie);
+    //setMySummary(json);
     setLoading(false);
   };
   useEffect(() => {
     getMovie();
   }, []);
-
+  //console.log(mySummary);
   return (
     <div>
-      <h1>Detail</h1>
+      <h1 className={style.detail}>Detail</h1>
       <hr></hr>
       {loading ? (
         <div>Hello! Now Loading...</div>
       ) : (
-        <div>
-          <img src={myjson.medium_cover_image} />
-          <h2>{myjson.title}</h2>
-          <h3>Genres</h3>
-          <ul>
-            {myjson.genres.map((g) => (
-              <li key={g}>{g}</li>
-            ))}
-          </ul>
-          <p>{myjson.date_uploaded}</p>
+        <div className={style.moviePage}>
+          <div>
+            <img className={style.movieImg} src={myjson.medium_cover_image} />
+          </div>
+          <div>
+            <h2 className={style.title}>{myjson.title}</h2>
+            <h3>Genres</h3>
+            <ul>
+              {myjson.genres.map((g) => (
+                <li key={g}>{g}</li>
+              ))}
+            </ul>
+            <p className={style.summary}>
+              {myjson.summary ? `${myjson.summary}` : `No Summary HERE`}
+            </p>
+            <p>{myjson.date_uploaded}</p>
+          </div>
         </div>
       )}
     </div>
